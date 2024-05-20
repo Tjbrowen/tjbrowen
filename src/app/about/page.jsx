e94207c59359612cc2b9edd032a0b725203cb24f
Tjbrowen <tjbrowen@gmail.com>
2024-05-20 15:55:12 +0200

    modified about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,63 @@
+"use client"
+import { motion } from "framer-motion";
+
 const AboutPage = () => {
-    return <div>AboutPage</div>;
+    return(
+    
+    
+    <motion.div
+     className="h-full" 
+     initial={{y:"-200vh"}}
+     animate={{y:"0%"}} 
+     transition={{duration: 1}}>
+     
+      {/* CONTAINER */}
+      <div className="">
+
+{/* TEXT CONTAINER */}
+<div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64">
+  
+  {/* BIOGRAPHY CONTAINER */}
+  <div className="flex flex-col gap-12 justify-center">
+    <hi>BIOGRAPHY </hi>
+
+     {/* BIOGRAPHY DEC*/}
+    <p>
+     I am an enthusiastic and results-driven web developer
+      with a strong background in creating and maintaining
+       websites and applications. My expertise lies in coding, 
+       debugging, and optimizing user experiences.
+      I am committed to ensuring seamless navigation
+       and delivering high-quality results
+   
+       </p>
+
+       <span>
+        I take responsibility on improving my skill on the web-development environment 
+       </span>
+
+         {/* BIOGRAPHY SIGN*/}
+        <div className=""></div>
+
+          {/* BIOGRAPHY SCROLL SVG*/}
+  </div>
+
+   {/* SKILLS CONTAINER */}
+   <div className="">SKILLS</div>
+
+    {/* EXPERIENCE CONTAINER */}
+  <div className="">EXPERIENCE</div>
+
+</div>
+
+
+{/* SVG CONTAINER */}
+<div className="hidden">
+
+</div>
+      </div>
+     </motion.div>
+    )
   };
   
   export default AboutPage;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,16 @@
+
+"use client"
 import Image from "next/image";
+import {motion} from "framer-motion"
 
 
 const Homepage = () => {
   return (
+    <motion.div
+     className="h-full" 
+     initial={{y:"-200vh"}}
+     animate={{y:"0%"}} 
+     transition={{duration: 1}}>
    <div className="h-full flex flex-col lg:flex-row gap-8 px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
     {/*IMAGE CONTAINER*/}
     <div className="h-1/2 lg:h-full lg:w-1/2 relative">
@@ -35,6 +43,7 @@ const Homepage = () => {
      </div>
   </div>
   </div>
+  </motion.div>
   );
 };
 
