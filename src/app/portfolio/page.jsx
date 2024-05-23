ac02dc3d6b1a3b7fef1b28e6d3093c957aa78e7d
Tjbrowen <tjbrowen@gmail.com>
2024-05-23 11:08:07 +0200

    changed forward security picture

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -18,7 +18,7 @@ const items = [
     color: "from-blue-300 to-violet-300",
     title: "4ward Security",
     desc: " Build on: Wordpress. Web Development, Design and Creative Design.",
-    img: " https://i.ibb.co/jbx2J9c/Project2.png",
+    img: " https://i.ibb.co/NZ70fkT/4wardsecurity.png",
     link: "https://4wardsecurity.co.za",
   },
  
