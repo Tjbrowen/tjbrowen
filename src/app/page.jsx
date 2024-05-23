a505964c909de51bedcd90fd72ab03b9c73dd41b
Tjbrowen <tjbrowen@gmail.com>
2024-05-23 09:39:06 +0200

    modified home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,7 +11,7 @@ const Homepage = () => {
      initial={{y:"-200vh"}}
      animate={{y:"0%"}} 
      transition={{duration: 1}}>
-   <div className="h-full flex flex-col lg:flex-row gap-8 px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+   <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
     {/*IMAGE CONTAINER*/}
     <div className="h-1/2 lg:h-full lg:w-1/2 relative">
   <Image src="/hero.png" alt="" fill className="object-contain" sizes="(max-width: 640px) 100vw, 50vw" />
